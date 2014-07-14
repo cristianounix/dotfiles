ecf08f961a7c74ec82340a8a344c0e443527e70b
cristianounix <cristianounix@gmail.com>
2014-07-14 10:53:05 -0300

    Script link files (install)

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/osx ~/.osx
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore ~/.gitignore
+ln -s ~/dotfiles/vim ~/.vim
