a43ac0f6ca29cc9fb82801300327114ed1a3e13a
cristianounix <cristianounix@gmail.com>
2014-08-20 11:29:00 -0300

    new configurations with zsh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/osx ~/.osx
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/gitignore ~/.gitignore
-ln -s ~/dotfiles/vim ~/.vim
