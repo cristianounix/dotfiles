1b3d6334b913f079eec2ee2e7eef53746a4fc0e1
cristianounix <cristianounix@gmail.com>
2014-08-22 22:59:14 -0300

    Setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if which brew > /dev/null; then
     echo 'HomeBrew ok !'
 else
@@ -12,6 +11,8 @@ brew install python
 brew install ack
 brew install mysql
 brew install rbenv && brew install ruby-build
+brew install macvim --override-system-vim
+brew install vim tmux
 
 # Install Ruby
 rbenv install 2.1.0
@@ -42,3 +43,12 @@ mkdir -p ~/.vim/bundle/NERD_tree-ack/plugin
 
 
 curl -L http://install.ohmyz.sh | sh
+
+
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/osx ~/.osx
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore ~/.gitignore
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
