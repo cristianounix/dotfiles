b14c1bd857ffe97c4d4d41c07fadfe457a7a546a
cristianounix <cristianounix@gmail.com>
2014-08-24 01:32:53 -0300

    Setup functions and brew cask

--- setup.sh
+++ setup.sh
@@ -1,54 +1,98 @@
 #!/bin/bash
 
-if which brew > /dev/null; then
+check_homebrew(){
+  if which brew > /dev/null; then
     echo 'HomeBrew ok !'
-else
+  else
     ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-fi
+  fi
+  brew tap phinze/homebrew-cask
+}
 
-brew install wget
-brew install python
-brew install ack fortune
-brew install mysql
-brew install rbenv && brew install ruby-build
-brew install macvim --override-system-vim
-brew install vim tmux
+list_apps_brew_cask(){
+    local -a list=(
+        "appcleaner"
+        "firefox"
+        "sublime-text"
+        "filezilla"
+        #"imageoptim"
+        "dropbox"
+        "mou"
+        #"sparrow"
+        #"feeds"
+        #"screenflow"
+        "limechat"
+        "arduino"
+        "google-drive"
+    )
+}
 
-# Install Ruby
-rbenv install 2.1.0
-rbenv global 2.1.0
+list_apps_brew(){
+  local -a list=(
+        "python"
+        "ack"
+        "fortune"
+        "vim"
+        "tmux"
+        "rbenv"
+        "ruby-build"
+        "mysql"
+        "macvim --override-system-vim"
+        "wget"
+        "htop"
+        "atop"
+        "brew-cask"
+  )
+}
 
-# Install MySQL
-mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
-mysql.server start
-/usr/local/Cellar/mysql/5.5.27/bin/mysqladmin -u root password '123'
-brew info mysql
+create_sym_links(){
+  ln -s ~/dotfiles/vimrc ~/.vimrc
+  ln -s ~/dotfiles/osx ~/.osx
+  ln -s ~/dotfiles/gitconfig ~/.gitconfig
+  ln -s ~/dotfiles/gitignore ~/.gitignore
+  ln -s ~/dotfiles/vim ~/.vim
+  ln -s ~/dotfiles/zshrc ~/.zshrc
+  ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+}
 
+vim_configs(){
+  pip install --user git+git://github.com/Lokaltog/powerline
+  cd ~/.vim/bundle/NERD_tree-ack/plugin
+  curl -so NERD_tree-ack.vim http://www.vim.org/scripts/download_script.php\?src_id\=17196
+  mkdir ~/.vim/bundle
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+  mkdir -p ~/.vim/bundle/NERD_tree-ack/plugin
+}
 
-pip install --user git+git://github.com/Lokaltog/powerline
+mysql_configs(){
+  # Install MySQL
+  mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
+  mysql.server start
+  #/usr/local/Cellar/mysql/5.5.27/bin/mysqladmin -u root password '123'
+  mysqladmin -u root password '123'
+  brew info mysql
+}
 
-cd ~/.vim/bundle/NERD_tree-ack/plugin
-curl -so NERD_tree-ack.vim http://www.vim.org/scripts/download_script.php\?src_id\=17196
+ohmyzsh_configs(){
+  curl -L http://install.ohmyz.sh | sh
+}
 
+ruby_configs(){
+  rbenv install 2.1.0
+  rbenv global 2.1.0
+}
 
-mkdir ~/.vim/bundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+check_homebrew
+exit 1
+brew install $(list_apps_brew)
+brew cask install $(list_apps_brew_cask)
 
+ruby_configs
+mysql_configs
+vim_configs
+ohmyzsh_configs
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-mkdir -p ~/.vim/bundle/NERD_tree-ack/plugin
-
-
-curl -L http://install.ohmyz.sh | sh
-
-
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/osx ~/.osx
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/gitignore ~/.gitignore
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+create_sym_links
